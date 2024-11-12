2c97fdeff168d13cfb19538b5cb1ff9dc4e49b23
Meda Varun Kumar <codedotgo@gmail.com>
2024-11-12 16:40:53 +0530

    repo as dependent class added to service class

--- Proj01-BootJpaCRUDOperations/src/main/java/com/cdc/service/DoctorMgmtServiceImpl.java
+++ Proj01-BootJpaCRUDOperations/src/main/java/com/cdc/service/DoctorMgmtServiceImpl.java
@@ -1,12 +1,15 @@
 package com.cdc.service;
 
 import com.cdc.entity.Doctor;
+import com.cdc.repository.IDoctorRepo;
 
 public class DoctorMgmtServiceImpl implements IDoctorService {
 
+	private IDoctorRepo doctorRepo;
+
 	@Override
 	public String registerDoctor(Doctor doctor) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
