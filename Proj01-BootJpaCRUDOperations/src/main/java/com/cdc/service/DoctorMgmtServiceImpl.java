7e027e46ba90e95a1a85cd17a07b89e557919a49
Meda Varun Kumar <codedotgo@gmail.com>
2024-11-12 16:39:08 +0530

    service interface and implement class added

---
+++ Proj01-BootJpaCRUDOperations/src/main/java/com/cdc/service/DoctorMgmtServiceImpl.java
@@ -0,0 +1,13 @@
+package com.cdc.service;
+
+import com.cdc.entity.Doctor;
+
+public class DoctorMgmtServiceImpl implements IDoctorService {
+
+	@Override
+	public String registerDoctor(Doctor doctor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ Proj01-BootJpaCRUDOperations/src/main/java/com/cdc/service/IDoctorService.java
@@ -0,0 +1,8 @@
+package com.cdc.service;
+
+import com.cdc.entity.Doctor;
+
+public interface IDoctorService {
+	public String registerDoctor(Doctor doctor);
+
+}
