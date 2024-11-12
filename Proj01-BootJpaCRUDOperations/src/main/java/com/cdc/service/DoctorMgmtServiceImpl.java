e54bff4895bef804003e2e6949319b5139145e25
Meda Varun Kumar <codedotgo@gmail.com>
2024-11-12 16:41:30 +0530

    repo as dependent class added to serviceimplclass

--- Proj01-BootJpaCRUDOperations/src/main/java/com/cdc/service/DoctorMgmtServiceImpl.java
+++ Proj01-BootJpaCRUDOperations/src/main/java/com/cdc/service/DoctorMgmtServiceImpl.java
@@ -1,10 +1,12 @@
 package com.cdc.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.cdc.entity.Doctor;
 import com.cdc.repository.IDoctorRepo;
 
 public class DoctorMgmtServiceImpl implements IDoctorService {
-
+	@Autowired
 	private IDoctorRepo doctorRepo;
 
 	@Override
