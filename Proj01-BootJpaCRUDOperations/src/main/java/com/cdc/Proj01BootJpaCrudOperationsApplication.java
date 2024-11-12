cd1f0f8792f8a9287f1dc3430841a90aed33e1b8
Meda Varun Kumar <codedotgo@gmail.com>
2024-11-12 16:53:22 +0530

    proj done

--- Proj01-BootJpaCRUDOperations/src/main/java/com/cdc/Proj01BootJpaCrudOperationsApplication.java
+++ Proj01-BootJpaCRUDOperations/src/main/java/com/cdc/Proj01BootJpaCrudOperationsApplication.java
@@ -2,12 +2,28 @@ package com.cdc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.cdc.entity.Doctor;
+import com.cdc.service.IDoctorService;
 
 @SpringBootApplication
 public class Proj01BootJpaCrudOperationsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Proj01BootJpaCrudOperationsApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(Proj01BootJpaCrudOperationsApplication.class, args);
+		IDoctorService service = ctx.getBean("doctorService", IDoctorService.class);
+		try {
+			Doctor doctor = new Doctor();
+			doctor.setDocName("Varun");
+			doctor.setDocSpecialization("Cardio");
+			doctor.setDocIncome(9900000.00);
+			String resultMsg = service.registerDoctor(doctor);
+			System.out.println(resultMsg);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		ctx.close();
 	}
 
 }
--- Proj01-BootJpaCRUDOperations/src/main/java/com/cdc/service/DoctorMgmtServiceImpl.java
+++ Proj01-BootJpaCRUDOperations/src/main/java/com/cdc/service/DoctorMgmtServiceImpl.java
@@ -1,10 +1,12 @@
 package com.cdc.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.cdc.entity.Doctor;
 import com.cdc.repository.IDoctorRepo;
 
+@Service("doctorService")
 public class DoctorMgmtServiceImpl implements IDoctorService {
 	@Autowired
 	private IDoctorRepo doctorRepo;
