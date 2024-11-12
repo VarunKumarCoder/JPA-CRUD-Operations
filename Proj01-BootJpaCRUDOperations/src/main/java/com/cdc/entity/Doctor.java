1dbb0739bae49ecaec76715c1498308408fdb992
Meda Varun Kumar <codedotgo@gmail.com>
2024-11-12 16:32:29 +0530

    @GeneratedValue annotation added

--- Proj01-BootJpaCRUDOperations/src/main/java/com/cdc/entity/Doctor.java
+++ Proj01-BootJpaCRUDOperations/src/main/java/com/cdc/entity/Doctor.java
@@ -2,6 +2,8 @@ package com.cdc.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -12,6 +14,7 @@ import lombok.Data;
 public class Doctor {
 	@Id
 	@Column(name = "DOC_ID")
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer docId;
 	@Column(name = "DOC_NAME", length = 25)
 	private String docName;
