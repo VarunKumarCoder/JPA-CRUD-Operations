9b40405cdb2a516ff833ae864d6b9d3677d6878f
Meda Varun Kumar <codedotgo@gmail.com>
2024-11-12 16:22:52 +0530

    JPACRUDoperations entity class started

---
+++ Proj01-BootJpaCRUDOperations/src/main/java/com/cdc/Proj01BootJpaCrudOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.cdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj01BootJpaCrudOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj01BootJpaCrudOperationsApplication.class, args);
+	}
+
+}
---
+++ Proj01-BootJpaCRUDOperations/src/main/java/com/cdc/entity/Doctor.java
@@ -0,0 +1,12 @@
+package com.cdc.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "JPA_DOCTOR_INFO")
+public class Doctor {
+
+}
